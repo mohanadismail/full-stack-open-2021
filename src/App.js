100979020140c9d7a1ccfbb7adcdb9d708e0c059
Mohanad <mohanadismail789@gmail.com>
2021-10-03 18:40:15 +0200

    Replaced total exercises with Total component

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ const App = () => {
       <Content part={part1} exercises={exercises1}/>
       <Content part={part2} exercises={exercises2}/>
       <Content part={part3} exercises={exercises3}/>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Total exercises={[exercises1, exercises2, exercises3]}/>
     </div>
   )
 }
