beeaf869500c589573b63322f12388d7f344b215
Mohanad <mohanadismail789@gmail.com>
2021-10-03 18:50:44 +0200

    Refactored Content component to render Part components instead of rendering contents directly

--- src/App.js
+++ src/App.js
@@ -8,11 +8,20 @@ const Header = (props) => {
   )
 }
 
-const Content = (props) => {
+const Part = (props) => {
   return (
     <p>
       {props.part} {props.exercises}
     </p>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <>
+    <Part part={props.parts[0]} exercises={props.exercises[0]} />
+    <Part part={props.parts[1]} exercises={props.exercises[1]}/>
+    <Part part={props.parts[2]} exercises={props.exercises[2]}/></>
   )
 }
 
@@ -32,13 +41,13 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+  const parts = [part1, part2, part3]
+  const exercises = [exercises1, exercises2, exercises3]
 
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content parts={parts} exercises={exercises}/>
       <Total exercises={[exercises1, exercises2, exercises3]}/>
     </div>
   )
