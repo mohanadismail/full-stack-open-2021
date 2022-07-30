fc0d0f40777e975c502d42689860c616befe31bf
Mohanad <mohanadismail789@gmail.com>
2022-07-30 12:45:37 +0200

    Create Statistics component

--- part1/exercises_1.6-1.11/src/App.js
+++ part1/exercises_1.6-1.11/src/App.js
@@ -4,6 +4,18 @@ const Button = ({name, handler}) => {
   return <button onClick={handler}>{name}</button>
 }
 
+const Statistics = ({good, neutral, bad}) => {
+  return <div>
+    <h1>statistics</h1>
+    <p>good {good}</p>
+    <p>neutral {neutral}</p>
+    <p>bad {bad}</p>
+    <p>all {good + neutral + bad}</p>
+    <p>average {(good - bad)/(good + bad + neutral)}</p>
+    <p>positive {(good/(good + bad + neutral)) * 100}%</p>
+  </div>
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -16,13 +28,7 @@ const App = () => {
       <Button name="good" handler={() => setGood(good + 1)}/>
       <Button name="neutral" handler={() => setNeutral(neutral + 1)}/>
       <Button name="bad" handler={() => setBad(bad + 1)}/>
-      <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good - bad)/(good + bad + neutral)}</p>
-      <p>positive {(good/(good + bad + neutral)) * 100}%</p>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
     </div>
   )
 }
